1b686d84a8112cbfc41ea1c2d5006e2cf48cd3f6
BharathChandana <bharathchandana43@gmail.com>
2024-12-16 00:11:22 +0530

    Code added

---
+++ WebApplicationExample13/WebApplicationExample13/Controllers/EmployeeController.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebApplicationExample13.Controllers
+{
+    public class EmployeeController:Controller
+    {
+        public ActionResult DisplayEmployees()
+        {
+            return View();
+        }
+    }
+}
---
+++ WebApplicationExample13/WebApplicationExample13/Services/MyLib.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using WebApplicationExample13.Models;
+
+namespace WebApplicationExample13.Services
+{
+    public class MyLib
+    {
+        public List<Employee> GetEmployeeList()
+        {
+            List<Employee> emplist = new List<Employee>()
+            {
+                new Employee()
+                {
+                    Id = 1,
+                    Name="Bharath",
+                    Department="Solutions",
+                    Designation="Developer",
+                    Salary=200000
+                },
+                new Employee()
+                {
+                    Id=2,
+                    Name="Kumar",
+                    Department="Microsoft",
+                    Designation="Tester",
+                    Salary=30000
+                }
+            };
+            return emplist;
+        }
+    }
+}
