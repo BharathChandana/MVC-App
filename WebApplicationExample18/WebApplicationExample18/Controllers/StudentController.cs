c17b2afcb6dfcf84f3b01adf4d01138e2387dfc2
BharathChandana <bharathchandana43@gmail.com>
2024-12-19 00:03:04 +0530

    Code Added

---
+++ WebApplicationExample18/WebApplicationExample18/Controllers/StudentController.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+using WebApplicationExample18.Models;
+using WebApplicationExample18.VM;
+
+namespace WebApplicationExample18.Controllers
+{
+    public class StudentController : Controller
+    {
+        
+        // GET: Student
+        public ActionResult Index()
+        {
+            
+            Student s = new Student()
+            {
+                StudentId = 1,
+                StudentName = "Bharath",
+                NameOfExam = "Final",
+                Standard = "10th",
+                s1 = 91,
+                s2=83,
+                s3=78,
+                s4=59,
+                s5=68,
+                s6=57
+            };
+            StudentVM svm=new StudentVM(s);
+            return View(svm);
+        }
+
+        
+    }
+}
---
+++ WebApplicationExample18/WebApplicationExample18/VM/StudentVM.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using WebApplicationExample18.Models;
+
+namespace WebApplicationExample18.VM
+{
+    public class StudentVM
+    {
+        public Student student;
+        public int Total;
+        public string Result;
+        public string Grade;
+        public int Average;
+        public string Colorname;
+        public string Color;
+        public StudentVM(Student s)
+        {
+            student = s;
+            Total = student.s1+student.s2+student.s3+student.s4+student.s5+student.s6;
+            Result = "Fail";
+            if(student.s1>=35&&student.s2>=35&&student.s3>=35&&student.s4>=35&&student.s5>=35&&student.s6>=35)
+            {
+                Result = "Pass";
+            }
+            int p = Total / 6;
+            if(Result=="Pass")
+            {
+                if(p>=90)
+                {
+                    Grade = "A+";
+                    Color = "Yellow";
+                } 
+                else if(p>=80)
+                {
+                    Grade = "A";
+                    Color = "Gray";
+                }
+                else if (p>=70)
+                {
+                    Grade = "B";
+                    Color = "Blue";
+                }
+                else if (p>=60)
+                {
+                    Grade = "C";
+                    Color = "orange";
+                }
+                else if (p>=50)
+                {
+                    Grade = "D";
+                    Color = "Pink";
+                }
+                else if (p>=35)
+                {
+                    Grade = "E";
+                    Color = "Lightblue";
+                }
+                
+            }
+            Average = Total / 6;
+            Colorname = Result == "Pass" ? "green" : "red";
+            
+        }
+    }
+}
